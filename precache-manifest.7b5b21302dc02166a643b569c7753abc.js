eeebef261922fdfde897d3a1345cbe57770db17b
Shakya47 <saurabhsh47g@gmail.com>
2020-07-11 11:52:21 +0530

    Updates

---
+++ precache-manifest.7b5b21302dc02166a643b569c7753abc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7f40ca5538a3c39bfcde2982788d79a",
+    "url": "/covid-19-tracker/index.html"
+  },
+  {
+    "revision": "eac83dfbcaa97cf151c2",
+    "url": "/covid-19-tracker/static/css/main.db327e38.chunk.css"
+  },
+  {
+    "revision": "387178f975a63225dfef",
+    "url": "/covid-19-tracker/static/js/2.ce06f8ff.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-19-tracker/static/js/2.ce06f8ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eac83dfbcaa97cf151c2",
+    "url": "/covid-19-tracker/static/js/main.bfd7c6ee.chunk.js"
+  },
+  {
+    "revision": "5bdbd92167a555c8ec71",
+    "url": "/covid-19-tracker/static/js/runtime-main.81159ed7.js"
+  },
+  {
+    "revision": "c3b9822d5cabdb79fa2cc6e848ff79dc",
+    "url": "/covid-19-tracker/static/media/image.c3b9822d.jpg"
+  }
+]);
